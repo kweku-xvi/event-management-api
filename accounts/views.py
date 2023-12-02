b5237e96180823d30938ea0312407f281259c629
kweku-xvi <nksarps@gmail.com>
2023-12-02 22:48:15 +0000

    add views for user creation and login

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,44 @@
+from .serializers import RegisterUserSerializer, LoginUserSerializer
+from django.contrib.auth import authenticate
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+@api_view(['POST'])
+def register_user_view(request):
+    if request.method == 'POST':
+        serializer = RegisterUserSerializer(data=request.data)
+
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+
+            return Response(
+                {
+                    'success':True,
+                    'data':serializer.data
+                }, status=status.HTTP_201_CREATED
+            )
+        return Response(
+            {
+                'success':False,
+                'message':serializer.errors
+            }, status=status.HTTP_400_BAD_REQUEST
+        )
+
+
+@api_view(['POST'])
+def login_user_view(request):
+    if request.method == 'POST':
+        serializer = LoginUserSerializer(data=request.data)
+
+        if serializer.is_valid(raise_exception=True):
+            tokens = serializer.generate_jwt_tokens(serializer.validated_data)
+
+            return Response(
+                {
+                    'success':True,
+                    'message':'Login successful!',
+                    'tokens':tokens
+                }, status=status.HTTP_200_OK
+            )
