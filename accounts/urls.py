6aec4cca9a85216f33033c81ea5b18c53e2a2ffe
kweku-xvi <nksarps@gmail.com>
2023-12-03 14:19:29 +0000

    add url endpoint for email verification

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,5 +4,7 @@ from django.urls import path
 
 urlpatterns = [
     path('register', views.register_user_view, name='register_user'),
+    path('verify-user', views.verify_user_view, name='verify_user'),
     path('login', views.login_user_view, name='login_user'),
+    path('users', views.get_users_view, name='get_all_users'),
 ]
