e7a62326831fde4a988965ca3e25984348a17546
kweku-xvi <nksarps@gmail.com>
2023-12-03 14:18:56 +0000

    add email verification

---
+++ accounts/utils.py
@@ -0,0 +1,21 @@
+import os
+from dotenv import load_dotenv
+from trycourier import Courier
+
+load_dotenv()
+
+client = Courier(auth_token=os.getenv('AUTH_TOKEN'))
+
+def send_verification_email(email, link, username):
+    client.send_message(
+        message={
+            "to": {
+            "email": email,
+            },
+            "template": "HQRFKDHDK84B16GJAQ7PWPFATXS8",
+            "data": {
+            "username": username,
+            "link": link,
+            },
+        }
+    )
